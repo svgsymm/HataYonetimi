88ee8b7d6deba676e3770e67a3e127d15c68fbeb
svgsymm <svgsym@hotmail.com>
2019-10-23 14:55:30 +0300

    first commit

--- HataYonetimi/Form1.Designer.cs
+++ HataYonetimi/Form1.Designer.cs
@@ -28,104 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnHataYonetimsiz = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.btnHataYonetimli = new System.Windows.Forms.Button();
-            this.btnHataYonetimliMesaj = new System.Windows.Forms.Button();
-            this.btnHataYonetimliMesaj1 = new System.Windows.Forms.Button();
-            this.btnHataYonetimliFinally = new System.Windows.Forms.Button();
-            this.btnExtra = new System.Windows.Forms.Button();
+            this.btnOrnek1 = new System.Windows.Forms.Button();
+            this.btnOrnek2 = new System.Windows.Forms.Button();
+            this.btnOrnek3 = new System.Windows.Forms.Button();
+            this.btnOrnek4 = new System.Windows.Forms.Button();
+            this.btnOrnek5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // btnHataYonetimsiz
+            // btnOrnek1
             // 
-            this.btnHataYonetimsiz.Location = new System.Drawing.Point(51, 36);
-            this.btnHataYonetimsiz.Name = "btnHataYonetimsiz";
-            this.btnHataYonetimsiz.Size = new System.Drawing.Size(208, 46);
-            this.btnHataYonetimsiz.TabIndex = 0;
-            this.btnHataYonetimsiz.Text = "Hata Yönetimsiz";
-            this.btnHataYonetimsiz.UseVisualStyleBackColor = true;
+            this.btnOrnek1.Location = new System.Drawing.Point(51, 36);
+            this.btnOrnek1.Name = "btnOrnek1";
+            this.btnOrnek1.Size = new System.Drawing.Size(208, 46);
+            this.btnOrnek1.TabIndex = 0;
+            this.btnOrnek1.Text = " Hata Yönetimsiz";
+            this.btnOrnek1.UseVisualStyleBackColor = true;
+            this.btnOrnek1.Click += new System.EventHandler(this.BtnHataYonetimsiz_Click);
             // 
-            // textBox1
+            // btnOrnek2
             // 
-            this.textBox1.Location = new System.Drawing.Point(551, 278);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
+            this.btnOrnek2.Location = new System.Drawing.Point(51, 88);
+            this.btnOrnek2.Name = "btnOrnek2";
+            this.btnOrnek2.Size = new System.Drawing.Size(208, 46);
+            this.btnOrnek2.TabIndex = 0;
+            this.btnOrnek2.Text = "Hata Yönetimli";
+            this.btnOrnek2.UseVisualStyleBackColor = true;
+            this.btnOrnek2.Click += new System.EventHandler(this.BtnHataYonetimli_Click);
             // 
-            // btnHataYonetimli
+            // btnOrnek3
             // 
-            this.btnHataYonetimli.Location = new System.Drawing.Point(51, 88);
-            this.btnHataYonetimli.Name = "btnHataYonetimli";
-            this.btnHataYonetimli.Size = new System.Drawing.Size(208, 46);
-            this.btnHataYonetimli.TabIndex = 0;
-            this.btnHataYonetimli.Text = "Hata Yönetimli";
-            this.btnHataYonetimli.UseVisualStyleBackColor = true;
+            this.btnOrnek3.Location = new System.Drawing.Point(51, 140);
+            this.btnOrnek3.Name = "btnOrnek3";
+            this.btnOrnek3.Size = new System.Drawing.Size(208, 46);
+            this.btnOrnek3.TabIndex = 0;
+            this.btnOrnek3.Text = "Hata Yönetimli - Mesaj";
+            this.btnOrnek3.UseVisualStyleBackColor = true;
+            this.btnOrnek3.Click += new System.EventHandler(this.BtnOrnek3_Click);
             // 
-            // btnHataYonetimliMesaj
+            // btnOrnek4
             // 
-            this.btnHataYonetimliMesaj.Location = new System.Drawing.Point(51, 140);
-            this.btnHataYonetimliMesaj.Name = "btnHataYonetimliMesaj";
-            this.btnHataYonetimliMesaj.Size = new System.Drawing.Size(208, 46);
-            this.btnHataYonetimliMesaj.TabIndex = 0;
-            this.btnHataYonetimliMesaj.Text = "Hata Yönetimli - Mesaj";
-            this.btnHataYonetimliMesaj.UseVisualStyleBackColor = true;
+            this.btnOrnek4.Location = new System.Drawing.Point(51, 192);
+            this.btnOrnek4.Name = "btnOrnek4";
+            this.btnOrnek4.Size = new System.Drawing.Size(208, 46);
+            this.btnOrnek4.TabIndex = 0;
+            this.btnOrnek4.Text = "Hata Yönetimli - Mesaj";
+            this.btnOrnek4.UseVisualStyleBackColor = true;
+            this.btnOrnek4.Click += new System.EventHandler(this.BtnOrnek4_Click);
             // 
-            // btnHataYonetimliMesaj1
+            // btnOrnek5
             // 
-            this.btnHataYonetimliMesaj1.Location = new System.Drawing.Point(51, 192);
-            this.btnHataYonetimliMesaj1.Name = "btnHataYonetimliMesaj1";
-            this.btnHataYonetimliMesaj1.Size = new System.Drawing.Size(208, 46);
-            this.btnHataYonetimliMesaj1.TabIndex = 0;
-            this.btnHataYonetimliMesaj1.Text = "Hata Yönetimli - Mesaj";
-            this.btnHataYonetimliMesaj1.UseVisualStyleBackColor = true;
-            // 
-            // btnHataYonetimliFinally
-            // 
-            this.btnHataYonetimliFinally.Location = new System.Drawing.Point(51, 244);
-            this.btnHataYonetimliFinally.Name = "btnHataYonetimliFinally";
-            this.btnHataYonetimliFinally.Size = new System.Drawing.Size(208, 46);
-            this.btnHataYonetimliFinally.TabIndex = 0;
-            this.btnHataYonetimliFinally.Text = "Hata Yönetimli - Finally";
-            this.btnHataYonetimliFinally.UseVisualStyleBackColor = true;
-            // 
-            // btnExtra
-            // 
-            this.btnExtra.Location = new System.Drawing.Point(51, 296);
-            this.btnExtra.Name = "btnExtra";
-            this.btnExtra.Size = new System.Drawing.Size(208, 46);
-            this.btnExtra.TabIndex = 0;
-            this.btnExtra.Text = "Extra";
-            this.btnExtra.UseVisualStyleBackColor = true;
+            this.btnOrnek5.Location = new System.Drawing.Point(51, 244);
+            this.btnOrnek5.Name = "btnOrnek5";
+            this.btnOrnek5.Size = new System.Drawing.Size(208, 46);
+            this.btnOrnek5.TabIndex = 0;
+            this.btnOrnek5.Text = "Hata Yönetimli - Finally";
+            this.btnOrnek5.UseVisualStyleBackColor = true;
+            this.btnOrnek5.Click += new System.EventHandler(this.BtnHataYonetimliFinally_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.btnExtra);
-            this.Controls.Add(this.btnHataYonetimliFinally);
-            this.Controls.Add(this.btnHataYonetimliMesaj1);
-            this.Controls.Add(this.btnHataYonetimliMesaj);
-            this.Controls.Add(this.btnHataYonetimli);
-            this.Controls.Add(this.btnHataYonetimsiz);
+            this.Controls.Add(this.btnOrnek5);
+            this.Controls.Add(this.btnOrnek4);
+            this.Controls.Add(this.btnOrnek3);
+            this.Controls.Add(this.btnOrnek2);
+            this.Controls.Add(this.btnOrnek1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnHataYonetimsiz;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button btnHataYonetimli;
-        private System.Windows.Forms.Button btnHataYonetimliMesaj;
-        private System.Windows.Forms.Button btnHataYonetimliMesaj1;
-        private System.Windows.Forms.Button btnHataYonetimliFinally;
-        private System.Windows.Forms.Button btnExtra;
+        private System.Windows.Forms.Button btnOrnek1;
+        private System.Windows.Forms.Button btnOrnek2;
+        private System.Windows.Forms.Button btnOrnek3;
+        private System.Windows.Forms.Button btnOrnek4;
+        private System.Windows.Forms.Button btnOrnek5;
     }
 }
 
--- HataYonetimi/Form1.cs
+++ HataYonetimi/Form1.cs
@@ -16,5 +16,84 @@ namespace HataYonetimi
         {
             InitializeComponent();
         }
-    }
-}
+
+        private void BtnHataYonetimsiz_Click(object sender, EventArgs e)
+        {
+            int telefonNumarasi = Convert.ToInt32(TextBox1.Text);
+            MessageBox.Show("Tebrikler, Telefon Numaranı sisteme girebildin! ");
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnHataYonetimli_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int telefonNumarasi = Convert.ToInt32(TextBox1.Text);
+                MessageBox.Show("Tebrikler, Telefon Numaranı sisteme girebildin! ");
+            }
+            catch
+            {
+                MessageBox.Show("Lütfen telefon numaranızı eksiksiz ve sadece sayısal verilerden oluşan bir formatta giriniz\nTelefon Formatı : 02123334455");
+
+            }
+        }
+
+        private void BtnOrnek3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int telefonNumarasi = Convert.ToInt32(TextBox1.Text);
+                MessageBox.Show("Tebrikler, Telefon Numaranı sisteme girebildin! ");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lütfen telefon numaranızı eksiksiz ve sadece sayısal verilerden oluşan bir formatta giriniz\nTelefon Formatı : 02123334455");
+            }
+        }
+
+        private void BtnOrnek4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int telefonNumarasi = Convert.ToInt32(TextBox1.Text);
+                MessageBox.Show("Tebrikler, Telefon Numaranı sisteme girebildin! ");
+            }
+            catch (DuplicateWaitObjectException)
+            {
+                MessageBox.Show("Lütfen telefon numaranızı eksiksiz ve sadece sayısal verilerden oluşan bir formatta giriniz\nTelefon Formatı : 02123334455");
+            }
+            catch (DivideByZeroException)
+            {
+                MessageBox.Show("Lütfen telefon numaranızı eksiksiz ve sadece sayısal verilerden oluşan bir formatta giriniz\nTelefon Formatı : 02123334455");
+            }
+            catch (DeletedRowInaccessibleException)
+            {
+                MessageBox.Show("Lütfen telefon numaranızı eksiksiz ve sadece sayısal verilerden oluşan bir formatta giriniz\nTelefon Formatı : 02123334455");
+            }
+            catch (DllNotFoundException)
+            {
+                MessageBox.Show("Lütfen telefon numaranızı eksiksiz ve sadece sayısal verilerden oluşan bir formatta giriniz\nTelefon Formatı : 02123334455");
+            }
+        }
+
+        private void BtnHataYonetimliFinally_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int telefonNumarasi = Convert.ToInt32(TextBox1.Text);
+                MessageBox.Show("Tebrikler, Telefon Numaranı sisteme girebildin! ");
+            }
+            catch 
+            {
+                MessageBox.Show("Lütfen telefon numaranızı eksiksiz ve sadece sayısal verilerden oluşan bir formatta giriniz\nTelefon Formatı : 02123334455");
+            }
+            finally
+            {
+                MessageBox.Show("Bu alan içerisinde yer alan kod blokları her zaman çalışacaktır.");
+            }
+        }
+    
