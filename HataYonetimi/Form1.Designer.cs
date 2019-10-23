e77c6b842d5682558ff4aa3d376ab060d8682887
svgsymm <svgsym@hotmail.com>
2019-10-23 13:10:53 +0300

    Add project files.

---
+++ HataYonetimi/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace HataYonetimi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHataYonetimsiz = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnHataYonetimli = new System.Windows.Forms.Button();
+            this.btnHataYonetimliMesaj = new System.Windows.Forms.Button();
+            this.btnHataYonetimliMesaj1 = new System.Windows.Forms.Button();
+            this.btnHataYonetimliFinally = new System.Windows.Forms.Button();
+            this.btnExtra = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnHataYonetimsiz
+            // 
+            this.btnHataYonetimsiz.Location = new System.Drawing.Point(51, 36);
+            this.btnHataYonetimsiz.Name = "btnHataYonetimsiz";
+            this.btnHataYonetimsiz.Size = new System.Drawing.Size(208, 46);
+            this.btnHataYonetimsiz.TabIndex = 0;
+            this.btnHataYonetimsiz.Text = "Hata Yönetimsiz";
+            this.btnHataYonetimsiz.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(551, 278);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnHataYonetimli
+            // 
+            this.btnHataYonetimli.Location = new System.Drawing.Point(51, 88);
+            this.btnHataYonetimli.Name = "btnHataYonetimli";
+            this.btnHataYonetimli.Size = new System.Drawing.Size(208, 46);
+            this.btnHataYonetimli.TabIndex = 0;
+            this.btnHataYonetimli.Text = "Hata Yönetimli";
+            this.btnHataYonetimli.UseVisualStyleBackColor = true;
+            // 
+            // btnHataYonetimliMesaj
+            // 
+            this.btnHataYonetimliMesaj.Location = new System.Drawing.Point(51, 140);
+            this.btnHataYonetimliMesaj.Name = "btnHataYonetimliMesaj";
+            this.btnHataYonetimliMesaj.Size = new System.Drawing.Size(208, 46);
+            this.btnHataYonetimliMesaj.TabIndex = 0;
+            this.btnHataYonetimliMesaj.Text = "Hata Yönetimli - Mesaj";
+            this.btnHataYonetimliMesaj.UseVisualStyleBackColor = true;
+            // 
+            // btnHataYonetimliMesaj1
+            // 
+            this.btnHataYonetimliMesaj1.Location = new System.Drawing.Point(51, 192);
+            this.btnHataYonetimliMesaj1.Name = "btnHataYonetimliMesaj1";
+            this.btnHataYonetimliMesaj1.Size = new System.Drawing.Size(208, 46);
+            this.btnHataYonetimliMesaj1.TabIndex = 0;
+            this.btnHataYonetimliMesaj1.Text = "Hata Yönetimli - Mesaj";
+            this.btnHataYonetimliMesaj1.UseVisualStyleBackColor = true;
+            // 
+            // btnHataYonetimliFinally
+            // 
+            this.btnHataYonetimliFinally.Location = new System.Drawing.Point(51, 244);
+            this.btnHataYonetimliFinally.Name = "btnHataYonetimliFinally";
+            this.btnHataYonetimliFinally.Size = new System.Drawing.Size(208, 46);
+            this.btnHataYonetimliFinally.TabIndex = 0;
+            this.btnHataYonetimliFinally.Text = "Hata Yönetimli - Finally";
+            this.btnHataYonetimliFinally.UseVisualStyleBackColor = true;
+            // 
+            // btnExtra
+            // 
+            this.btnExtra.Location = new System.Drawing.Point(51, 296);
+            this.btnExtra.Name = "btnExtra";
+            this.btnExtra.Size = new System.Drawing.Size(208, 46);
+            this.btnExtra.TabIndex = 0;
+            this.btnExtra.Text = "Extra";
+            this.btnExtra.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnExtra);
+            this.Controls.Add(this.btnHataYonetimliFinally);
+            this.Controls.Add(this.btnHataYonetimliMesaj1);
+            this.Controls.Add(this.btnHataYonetimliMesaj);
+            this.Controls.Add(this.btnHataYonetimli);
+            this.Controls.Add(this.btnHataYonetimsiz);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHataYonetimsiz;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnHataYonetimli;
+        private System.Windows.Forms.Button btnHataYonetimliMesaj;
+        private System.Windows.Forms.Button btnHataYonetimliMesaj1;
+        private System.Windows.Forms.Button btnHataYonetimliFinally;
+        private System.Windows.Forms.Button btnExtra;
+    }
+}
+
